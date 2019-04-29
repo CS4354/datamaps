d5b21647b071312711cce051cc90b1a0b90526c2
Annie Vo <annievo.av@hotmail.com>
2019-04-29 04:33:50 -0500

    bubbles and layout done

--- data.js
+++ data.js
@@ -1,231 +1,14 @@
 var map = new Datamap({
-  element: document.getElementById('container'),
+  element: document.getElementById('pollution-map'),
   scope: 'usa',
   geographyConfig: {
     popupOnHover: false,
     highlightOnHover: false,
+    responsive: true,
     borderWidth: 1,
     // dataUrl: 'https://rawgit.com/Anujarya300/bubble_maps/master/data/geography-data/canada.topo.json',
-    // popupTemplate: function (geo, data) {
-    //     return '<div class="hoverinfo"><strong>' + geo.properties.name + '</strong></div>';
-    // }
   },
-  // fills: {
-  //   'Republican': '#CC4731',
-  //   'Democrat': '#306596',
-  //   'Heavy Democrat': '#667FAF',
-  //   'Light Democrat': '#A9C0DE',
-  //   'Heavy Republican': '#CA5E5B',
-  //   'Light Republican': '#EAA9A8',
-  //   defaultFill: '#EDDC4E'
-  // },
-  // data: {
-  //   "AZ": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 5
-  //   },
-  //   "CO": {
-  //     "fillKey": "Light Democrat",
-  //     "electoralVotes": 5
-  //   },
-  //   "DE": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "FL": {
-  //     "fillKey": "UNDECIDED",
-  //     "electoralVotes": 29
-  //   },
-  //   "GA": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "HI": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "ID": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "IL": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "IN": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 11
-  //   },
-  //   "IA": {
-  //     "fillKey": "Light Democrat",
-  //     "electoralVotes": 11
-  //   },
-  //   "KS": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "KY": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "LA": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "MD": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "ME": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "MA": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "MN": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "MI": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "MS": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "MO": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 13
-  //   },
-  //   "MT": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "NC": {
-  //     "fillKey": "Light Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "NE": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "NV": {
-  //     "fillKey": "Heavy Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "NH": {
-  //     "fillKey": "Light Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "NJ": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "NY": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "ND": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "NM": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "OH": {
-  //     "fillKey": "UNDECIDED",
-  //     "electoralVotes": 32
-  //   },
-  //   "OK": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "OR": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "PA": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "RI": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "SC": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "SD": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "TN": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "TX": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "UT": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "WI": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "VA": {
-  //     "fillKey": "Light Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "VT": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "WA": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "WV": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "WY": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "CA": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "CT": {
-  //     "fillKey": "Democrat",
-  //     "electoralVotes": 32
-  //   },
-  //   "AK": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "AR": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  //   "AL": {
-  //     "fillKey": "Republican",
-  //     "electoralVotes": 32
-  //   },
-  // },
-
 });
-
-// State Label
-map.labels();
 
 var airData = [{
     radius: 2,
@@ -241,12 +24,39 @@ var airData = [{
   }
 ];
 
-//draw bubbles for bombs
+// draw bubbles for airData
 map.bubbles(airData, {
   bubblesConfig: {
     popupOnHover: true,
     popupTemplate: function (geo, data) {
-    return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
+      return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
     }
   }
 });
+
+var customResize = function () {
+  if (window.matchMedia("(max-width: 440px)").matches) {
+    map.resize();
+    map.labels({
+      fontSize: 6
+    });
+  } else if (("(max-width: 660px)").matches) {
+    map.resize();
+    map.labels({
+      fontSize: 8
+    });
+  } else if (window.matchMedia("(max-width: 990px)").matches) {
+    map.resize();
+    map.labels({
+      fontSize: 10
+    });
+  } else {
+    map.resize();
+    map.labels({
+      fontSize: 12
+    });
+  }
+};
+
+// State labels size
+window.addEventListener('resize', customResize());
